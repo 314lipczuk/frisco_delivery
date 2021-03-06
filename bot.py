9a7b17f600ec908d54798fb1d32ec72b9e0e4d51
314lipczuk <3.14lipczuk@gmail.com>
2021-03-06 17:04:13 +0100

    Proper timezone handling(well not really but it works)

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-#from seleniumwire import webdriver
-from selenium import webdriver
+from seleniumwire import webdriver
+from pytz import timezone
+#from selenium import webdriver
 import sys
 import time
 import requests
@@ -10,7 +11,6 @@ import datetime
 ##TODO
 
 ##remake generateVanReq so that it is not retarded
-##Create actual control flow
 ##Make it actually work
 
 
@@ -35,11 +35,15 @@ class friscoBot():
         driver.find_elements_by_name("username")[0].send_keys(self.email)
         driver.find_elements_by_id("loginPassword")[0].send_keys(self.passw)
         driver.find_elements_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/section/button')[0].click()
+        time.sleep(1)
         return driver
 
     def generateVanRequest(self, driver):
+        if driver.find_elements_by_class_name('ps-active-y') == []:
+            driver.find_element_by_class_name('header-delivery').click()
+            time.sleep(3)
         try:
-            driver.find_elements_by_xpath("//div[@class='day active']")[0].click()
+            #driver.find_elements_by_xpath("//div[@class='day active']")[0].click()
             days = driver.find_elements_by_class_name('day')
             for d in days:
                 d.click()
@@ -47,34 +51,36 @@ class friscoBot():
             print('Some error in calling van via ui')
             pass
         time.sleep(0.5)
-        vanfound = False
         nr = 1
-        while vanfound==False:
+        req = None
+        while req is None:
             if nr>50:
                 print('Exceeded limit of requests while searching for van, calling it prob doesnt work')
-                break
+                print('Going into recursion')
+                req =self.generateVanRequest(driver) 
             if driver.requests[nr*-1].path == "/app/commerce/api/v1/users/590820/calendar/Van":
-                vanfound = True
+                print('found van request')
+                req=driver.requests[-1*nr]
             else:
                 nr+=1
-        return driver.requests[-1*nr]
+        return req
 
 
     def checkSchedule(self, driver):
         time.sleep(1)
         driver.find_element_by_class_name('header-delivery').click()
-        time.sleep(1)
+        time.sleep(3)
         van_req = self.generateVanRequest(driver)
         print(van_req.response)
         r= requests.get(van_req.url,headers=van_req.headers )
         jsn=json.loads(r.text)
+        self.log(jsn['firstOpenWindow']['deliveryWindow']['startsAt'])
         return datetime.datetime.fromisoformat(jsn['firstOpenWindow']['deliveryWindow']['startsAt'])
         
     def verifyTimes(self, sch):
-        now = datetime.datetime.now()
         if(int(sys.argv[1])<=1):
             raise ValueError
-        now = datetime.datetime.now()
+        now = datetime.datetime.now(timezone('Europe/Warsaw'))
         allowed_td = datetime.timedelta(days=int(sys.argv[1]))
         if sch-now >= datetime.timedelta(days=1) and sch- allowed_td >= now:
             print('Found')
@@ -86,9 +92,13 @@ class friscoBot():
         driver = self.login()
         reserved = False
         while not reserved:
-            sch = checkSchedule(driver)
-            if verifyTimes():
-                print('ending')
+            sch = self.checkSchedule(driver)
+            if self.verifyTimes(sch):
+                print('passed:ending')
+                reserved = True
+            else:
+                sleep(60)
+                print('rumble again')
 
 
 
