38e9ea9b362d2f398260cd89dcc5583290cfd2c4
314lipczuk <3.14lipczuk@gmail.com>
2021-03-27 20:20:31 +0100

    Changes to match website change

--- bot.py
+++ bot.py
@@ -7,11 +7,9 @@ import requests
 import brotli
 import json
 import datetime
-
+import re
 ##TODO
-
 ##Make it actually work
-
 
 class friscoBot():
     f = open('.pb.txt', 'r')
@@ -21,26 +19,23 @@ class friscoBot():
     email = crds[0]
     passw = crds[1]
     van_Request = None
+    dayfocused = None
 
     def log(self, mes):
         f=open('log.txt','a')
-        f.write(mes)
+        f.write('\n'+mes)
         f.close()
 
     def login(self):
-        try:
-            driver = webdriver.Chrome()
-            driver.get("https://www.frisco.pl/")
-            driver.find_element_by_xpath('//*[@id="header"]/div[1]/div/div[3]/div/a[1]').click()
-            time.sleep(3)
-            driver.find_elements_by_name("username")[0].send_keys(self.email)
-            driver.find_elements_by_id("loginPassword")[0].send_keys(self.passw)
-            driver.find_elements_by_xpath('/html/body/div[1]/div[2]/div/div[2]/div/div/form/section/button')[0].click()
-            time.sleep(2)
-            driver.find_element_by_class_name('header-delivery')
-        except: 
-            driver.quit()
-            driver = self.login()
+        driver = webdriver.Chrome()
+        driver.get("https://www.frisco.pl/login")
+        time.sleep(3)
+        #driver.find_element_by_xpath('i//*[@id="header"]/div[1]/div/div[3]/div/button[1]').click()
+        #time.sleep(3)
+        driver.find_element_by_id("email").send_keys(self.email)
+        driver.find_element_by_id("password").send_keys(self.passw)
+        driver.find_element_by_class_name("button").click()
+        time.sleep(2)
         return driver
 
     def generateVanRequest(self, driver):
@@ -80,46 +75,60 @@ class friscoBot():
         r= requests.get(van_req.url,headers=van_req.headers )
         jsn=json.loads(r.text)
         self.log(jsn['firstOpenWindow']['deliveryWindow']['startsAt'])
+        print("checkSchedule: "+ jsn['firstOpenWindow']['deliveryWindow']['startsAt'])
         return datetime.datetime.fromisoformat(jsn['firstOpenWindow']['deliveryWindow']['startsAt'])
         
     def verifyTimes(self, sch):
         now = datetime.datetime.now(timezone('Europe/Warsaw'))
-        print(now)
-        print(sch)
-        allowed_td = datetime.timedelta(days=int(sys.argv[1]))
-        if sch- allowed_td >= now:
+        print("verifyTimes: now:"+ str(now)+" | sch: "+ str(sch))
+        allowed_td = datetime.timedelta(int(sys.argv[1]))
+        if sch - now <=allowed_td:
             print('Found')
+            self.dayfocused = sch.day
             return True
         else:
             return False
+
 
     def acceptDelivery(self,driver):
         if driver.find_elements_by_class_name('ps-active-y') != []:
             driver.find_element_by_class_name('header-delivery').click()
             time.sleep(3)
-        driver.find_element_by_class_name('header-delivery').click()
+        else:
+            driver.find_element_by_class_name('header-delivery').click()
+            time.sleep(2)
+            driver.find_element_by_class_name('header-delivery').click()
         time.sleep(2)
         driver.find_element_by_class_name('available').click()
-        driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[2]/div[2]/div/div/div[3]/div[2]/div/div[2]").click()
-        print('there was an attempt') 
+        days = driver.find_element_by_class_name('days-inner')
+        selected =days.find_element_by_class_name('active')
 
+        num = re.findall('[0-9]+', selected.text)
+
+        
+        print("selected: "+str(num)+" matched: "+str(self.dayfocused))
+        if self.dayfocused in num:
+            print("selefted: "+str(num)+"matched: "+str(self.dayfocused))
+            #driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/div/div[1]/div[2]/div[2]/div[2]/div/div/div[3]/div[2]/div/div[2]").click()
+        else:
+            print('shit fucced')
     def run(self):
         if(int(sys.argv[1])<=1):
             raise ValueError
         driver = self.login()
         self.generateVanRequest(driver)
         reserved = False
-        while not reserved:
-            print('inside waiting for a suitable date')
+        while reserved != True:
+            print('run: beginning of while loop')
             sch = self.checkSchedule(driver)
             if self.verifyTimes(sch):
                 print('passed:ending')
+                self.acceptDelivery(driver)
                 reserved = True
             else:
-                print('goin to sleep, ')
-                print(sch )
+                print('run: goin to sleep, ')
                 time.sleep(60)
-                print('rumble again')
+                print('run: end of sleep')
         time.sleep(300)
 
 if __name__ == "__main__":
